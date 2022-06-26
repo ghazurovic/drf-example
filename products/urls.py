10d39690904aed1a7dda601773c9baa4476b7c62
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 18:52:53 +0200

    Add ProductReview url.

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,15 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProductsViewSet
+from .views import ProductsViewSet, ProductReviewsViewSet
 from django.urls import path, include
 
 
 router = DefaultRouter()
 router.register(r'products', ProductsViewSet, basename='products')
+router.register(
+    r'product-reviews',
+    ProductReviewsViewSet,
+    basename='product-reviews'
+)
 
 products_api = [
     path('', include(router.urls)),
