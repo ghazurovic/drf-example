221b0c88cf4bb4ebdad62abf9fe238dee14fed7d
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 18:51:56 +0200

    Add ProductReview model.

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
@@ -21,3 +22,28 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class ProductReview(models.Model):
+    class Meta:
+        unique_together = ('product', 'user')
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    comment = models.CharField(
+        max_length=500,
+        null=True,
+        blank=True
+    )
+    product_rating = models.FloatField(
+        default=0,
+        validators=[MinValueValidator(0.0), MaxValueValidator(5.0)],
+    )
