880f83b9115b65a1c529c424e614bbb254284498
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 16:37:43 +0200

    Override product viewset methods.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,10 @@
-from rest_framework import viewsets, status, filters, generics
+import typing
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.db.models.functions import Lower
+from django.db.models.query import QuerySet
+from core.helpers.model_manager import get_unique_or_none
 from .models import Product
 from .serializers import ProductSerializer
 from core.helpers.pagination import StandardResultsSetPagination
@@ -22,10 +27,19 @@ class ProductsViewSet(viewsets.ModelViewSet):
         'name',
         'price',
         'rating',
-        'updated_at'
+        'updated_at',
     )
 
-    def get_permissions(self):
+    ordering_fields = (
+        'name',
+        'price',
+        'rating',
+        'updated_at',
+    )
+
+    ordering = ('name', )
+
+    def get_permissions(self) -> typing.List:
         """
         Instantiates and returns the list of permissions that this view
         requires.
@@ -35,3 +49,60 @@ class ProductsViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [IsAuthenticated, ]
         return [permission() for permission in permission_classes]
+
+    def not_found(self) -> typing.Optional[Response]:
+        return Response(
+            {'error': 'Product not found.'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    def get_object(self) -> typing.Optional[Product]:
+        return get_unique_or_none(Product, id=self.kwargs.get('pk'))
+
+    def get_ordered_queryset(
+        self,
+        qs: QuerySet,
+        initial_order: str
+    ) -> typing.Optional[QuerySet]:
+        """Override order queryset to support case insensitive ordering.
+
+        Args:
+            qs (QuerySet): _description_
+            initial_order (str): initial order to be applied
+
+        Returns:
+            QuerySet: sorted QuerySet
+        """
+        order_by = self.request.query_params.get('ordering')
+
+        if not order_by:
+            order_by = initial_order
+
+        if order_by.startswith('-'):
+            return sorted(qs.order_by(
+                Lower(order_by[1:])),
+                key=lambda x: x.__dict__[order_by[1:]].lower(),
+                reverse=True
+            )
+        return qs.order_by(Lower(order_by))
+
+    def list(self, request) -> typing.Optional[QuerySet]:
+        """Return list of all products as pagination response or """
+        try:
+            queryset = self.get_ordered_queryset(
+                self.filter_queryset(self.get_queryset()),
+                'name'
+            )
+
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response(
+                {'error': e},
+                status=status.HTTP_400_BAD_REQUEST
+            )
