0db1f4498be92e369b8913fd48b3c66ede326e7f
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 15:26:11 +0200

    Add serializer validator for rating.

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import Product
+from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -11,3 +11,8 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'rating'
         )
+
+    def validate_rating(self, value):
+        if value < 0 or value > 5:
+            raise serializers.ValidationError('Rating has to be between 1 and 10.')
+        return value
