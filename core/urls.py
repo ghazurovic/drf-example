1badfb6a69955d84c3f58d7c148b3086b2aa21e3
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 15:27:31 +0200

    Update urls.

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,47 @@
 """core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from products.urls import products_api
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="DRF Example API",
+      default_version='v1',
+      description="DRF Example Open API spec",
+      terms_of_service=""
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+swagger_api = [
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
+]
+
+api_urls = [
+    path('', include(products_api)),
+]
 
 urlpatterns = [
+    path('', include(swagger_api)),
     path('admin/', admin.site.urls),
+    path('api/', include(api_urls)),
 ]
