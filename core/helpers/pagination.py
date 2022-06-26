c879c6dd761bc628b0d37a5e62fe803cf4bab7ce
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 15:34:22 +0200

    Flake8 on pagination.

--- core/helpers/pagination.py
+++ core/helpers/pagination.py
@@ -10,7 +10,10 @@ class StandardResultsSetPagination(pagination.PageNumberPagination):
     max_page_size = 20
     actual_page_size = 5
 
-    def get_paginated_response(self, data: QuerySet) -> typing.Optional[Response]:
+    def get_paginated_response(
+        self,
+        data: QuerySet
+    ) -> typing.Optional[Response]:
         """Transforms paginated response to more useful one with
         additional keys such as current_page, last_page, total_pages,
         from and to.
@@ -20,7 +23,10 @@ class StandardResultsSetPagination(pagination.PageNumberPagination):
             Response: paginated response
         """
         _page_size_param = self.request.query_params.get('page_size')
-        self.actual_page_size = int(_page_size_param) if int(_page_size_param) != self.page_size else self.page_size
+        if int(_page_size_param) != self.page_size:
+            self.actual_page_size = int(_page_size_param)
+        else:
+            self.actual_page_size = self.page_size
         return Response({
             'links': {
                 'next': self.get_next_link(),
@@ -37,7 +43,8 @@ class StandardResultsSetPagination(pagination.PageNumberPagination):
         })
 
     def calc_from_page(self):
-        _from_page = self.actual_page_size * self.page.number - (self.actual_page_size - 1)
+        _from_page = self.actual_page_size * self.page.number - \
+            (self.actual_page_size - 1)
         if isinstance(_from_page, tuple):
             return _from_page[0]
 
@@ -46,8 +53,10 @@ class StandardResultsSetPagination(pagination.PageNumberPagination):
     def calc_to_page(self):
         _to_page = int(self.actual_page_size * self.page.number)
         if isinstance(_to_page, tuple):
-            _to = _to_page[0] if _to_page[0] < self.page.paginator.count else self.page.paginator.count
+            _to = _to_page[0] if _to_page[0] < self.page.paginator.count \
+                else self.page.paginator.count
             return _to
-        _to = _to_page if _to_page < self.page.paginator.count else self.page.paginator.count
+        _to = _to_page if _to_page < self.page.paginator.count \
+            else self.page.paginator.count
 
         return _to
