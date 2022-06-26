ced03db52a8380e42342b0a4650ac50451ffbe8a
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 15:26:32 +0200

    Include typings in model manager.

--- core/helpers/model_manager.py
+++ core/helpers/model_manager.py
@@ -1,10 +1,12 @@
+from django.db.models import Model
+
 
 def get_unique_or_none(model, **kwargs):
     """Gets unique or None for provided model.
     Args:
-        model (_type_): Model on which query should be performed.
+        model (Model): Model on which query should be performed.
     Returns:
-        _type_: Result of query as signle instance or None.
+        Model: Result of query as signle instance or None.
     """
     try:
         return model.objects.get(**kwargs)
