501e409460970c9e040c32853758b354d8f78d8f
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 15:27:19 +0200

    Add basic products viewset.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, filters, generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Product
+from .serializers import ProductSerializer
+from core.helpers.pagination import StandardResultsSetPagination
 
-# Create your views here.
+
+class ProductsViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing, creating, editing and deleting Product instances.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = StandardResultsSetPagination
+
+    filter_backends = (
+        filters.SearchFilter,
+        filters.OrderingFilter
+    )
+
+    search_fields = (
+        'name',
+        'price',
+        'rating',
+        'updated_at'
+    )
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view
+        requires.
+        """
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [AllowAny, ]
+        else:
+            permission_classes = [IsAuthenticated, ]
+        return [permission() for permission in permission_classes]
