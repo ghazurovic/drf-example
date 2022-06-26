5435dfe9b80199fb9e95844faa146e005ed8ae75
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 14:00:13 +0200

    Add product model.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        help_text='Name of the product',
+        unique=True
+    )
+    price = models.DecimalField(
+        decimal_places=2,
+        max_digits=20,
+        help_text='Product price.'
+    )
+    rating = models.FloatField(
+        default=0,
+        validators=[MinValueValidator(0.0), MaxValueValidator(5.0)],
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
