42e957535476019cef16d29bf5ffc3ca36712eac
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 16:37:27 +0200

    Cleanup.

--- products/serializers.py
+++ products/serializers.py
@@ -3,18 +3,13 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    rating = serializers.FloatField(read_only=True)
+
     class Meta:
         model = Product
         fields = (
             'id',
             'name',
             'price',
-            'rating'
+            'rating',
         )
-
-    def validate_rating(self, value):
-        if value < 0 or value > 5:
-            raise serializers.ValidationError(
-                'Rating has to be between 0 and 5.'
-            )
-        return value
