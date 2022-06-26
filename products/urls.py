8d631858cb953661e96d73e255efc4ff8b426c33
Goran Hazurovic <goran.hazurovic@semapp.de>
2022-06-26 15:27:50 +0200

    Update products urls.

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProductsViewSet
+from django.urls import path, include
+
+
+router = DefaultRouter()
+router.register(r'products', ProductsViewSet, basename='products')
+
+products_api = [
+    path('products/', include(router.urls)),
+]
